96c593c434f124248d05c1977f69637104fbf504
park <90lastday@gmail.com>
2023-01-06 14:31:28 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import SignatureCanvas from 'react-signature-canvas';
+import './Styles/style.css';
 
 function App() {
+  const signCanvas = useRef() as React.MutableRefObject<any>;
+  const clear = () => {
+    signCanvas.current.clear();
+  }
+  const save = () => {
+    const image = signCanvas.current.getTrimmedCanvas().toDataURL('image/png');
+    const link = document.createElement('a');
+    link.href = image;
+    link.download = 'sign_image.png';
+    link.click();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1> 서명 연습 </h1>
+      <div className='container'>
+        <div className='canvasContainer'>
+          <SignatureCanvas
+            ref={signCanvas}
+            canvasProps={{ className: 'signCanvas canvasStyle '}}
+            backgroundColor="rgb(230, 230, 230)"
+          />
+        </div>
+        <button onClick={clear}>clear</button>
+        <button onClick={save}>save</button>
+      </div>
+    </>
   );
 }
 
